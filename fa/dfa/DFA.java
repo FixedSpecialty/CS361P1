9fee3c2962ad93b1a892e6d970ed749c4d3c3ef5
Andre Murphy <andremurphy98@outlook.com>
2020-09-12 17:41:14 -0600

    added package files and created missing classes

---
+++ fa/dfa/DFA.java
@@ -0,0 +1,35 @@
+package fa.dfa;
+
+public class DFA {
+
+	public void addFinalState(String nextToken) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addStartState(String startStateName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addState(String nextToken) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addTransition(String valueOf, char c, String valueOf2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public DFA complement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean accepts(String input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
