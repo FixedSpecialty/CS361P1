36346d1da1a4fbf294cb328584e8ab889b71bdbf
joshdixon0515 <43324427+joshdixon0515@users.noreply.github.com>
2020-09-22 19:28:49 -0600

    Night 1 edit 1. Cheers.

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -1,35 +1,105 @@
 package fa.dfa;
 
-public class DFA {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
-	public void addFinalState(String nextToken) {
-		// TODO Auto-generated method stub
-		
+import fa.State;
+import javafx.util.Pair;
+
+public class DFA implements DFAInterface {
+
+	private String initial;
+	private Set<DFAState> states;
+	private Set<Character> alphabet;
+	
+	public DFA(){
+	
+	}
+
+
+	public void addFinalState(String nextToken) {		
+		DFAState newState = new DFAState(nextToken);
+		newState.setFinal(true);
+		states.add(newState);
 	}
 
 	public void addStartState(String startStateName) {
-		// TODO Auto-generated method stub
-		
+		DFAState newState = new DFAState(startStateName);
+		newState.setInit(true);
+		states.add(newState);
+		initial = startStateName;
 	}
 
 	public void addState(String nextToken) {
-		// TODO Auto-generated method stub
-		
+		DFAState newState = new DFAState(nextToken);
+		states.add(newState);
 	}
 
 	public void addTransition(String valueOf, char c, String valueOf2) {
-		// TODO Auto-generated method stub
-		
+		alphabet.add(c);
+		Iterator<DFAState> iter = states.iterator();
+		DFAState currState;
+		while(iter.hasNext()){
+			currState  = iter.next();
+			if(currState.getName() == valueOf){
+				currState.addTransition(c, valueOf2);
+			}
+		}
 	}
 
 	public DFA complement() {
-		// TODO Auto-generated method stub
-		return null;
+		DFA dfa = this;
+		Set<? extends State> states = dfa.getStates();
+		Iterator<? extends State> iter = states.iterator();
+		DFAState curr;
+		while(iter.hasNext()){
+			curr = (DFAState)iter.next();
+			if(curr.isFinal()){
+				curr.setFinal(false);
+			}
+			else{
+				curr.setFinal(true);
+			}
+		}
+		return dfa;
 	}
 
 	public boolean accepts(String input) {
-		// TODO Auto-generated method stub
+
 		return false;
+	}
+
+	@Override
+	public Set<? extends State> getStates() {
+
+		return states;
+	}
+
+	@Override
+	public Set<? extends State> getFinalStates() {
+
+		return null;
+	}
+
+	@Override
+	public State getStartState() {
+
+		return null;
+	}
+
+	@Override
+	public Set<Character> getABC() {
+
+		return null;
+	}
+
+	@Override
+	public State getToState(DFAState from, char onSymb) {
+
+		return null;
 	}
 
 }
--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -1,5 +1,58 @@
 package fa.dfa;
 
-public class DFAState {
+import java.util.*;
+import javafx.util.Pair;
 
+public class DFAState extends fa.State{
+    
+    private boolean finals;
+    private boolean init;
+    private Set<Pair<Character, String>> transitions;
+
+    /**
+     * Constructor. Used to set up state with name as well as whether
+     * this is a final state and or initial state.
+     * 
+     * @param token - name of state.
+     */
+    public DFAState(String token){
+        name = token;
+        init = false;
+        finals = false;
+    }
+    
+    public void addTransition(char c, String a){
+        Pair<Character, String> pair = new Pair<Character, String>(c,a);
+        transitions.add(pair);
+    }
+
+    public String getTransitionTo(Character c){
+        Iterator<Pair<Character, String>> iter = transitions.iterator();
+        Pair<Character, String> pair;
+        while(iter.hasNext()){
+           pair = iter.next();
+           if(pair.getKey() == c){
+                return pair.getValue();
+           }
+        }
+        return null;
+    }
+
+    public boolean isInit(){
+        return init;
+    }
+
+    public void setInit(boolean bool){
+        init = bool;
+    }
+
+
+    public boolean isFinal()
+    {
+        return finals;
+    }    
+
+    public void setFinal(boolean bool){
+        finals = bool;
+    }
 }
